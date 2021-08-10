634a4932ee3b0712357a6a97bfa883000d07c0b8
Soonki <alstnsrl98@gmail.com>
2021-08-10 14:43:42 +0900

    Delete(soonki/home): pages폴더에서 main.js 삭제

--- src/components/main/ServiceLogo.js
+++ src/components/main/ServiceLogo.js
@@ -2,7 +2,17 @@ import React from "react";
 import styles from "./ServiceLogo.module.css";
 
 function ServiceLogo() {
-  return <div className={styles.container}>hello</div>;
+  return (
+    <div className={styles.container}>
+      <h1>S</h1>
+      <h1>E</h1>
+      <h1>R</h1>
+      <h1>V</h1>
+      <h1>I</h1>
+      <h1>C</h1>
+      <h1>E</h1>
+    </div>
+  );
 }
 
 export default ServiceLogo;
--- src/pages/main.js
+++
@@ -1,7 +0,0 @@
-import ServiceLogo from "../components/main/ServiceLogo";
-
-function Main() {
-  return <ServiceLogo />;
-}
-
-export default Main;
