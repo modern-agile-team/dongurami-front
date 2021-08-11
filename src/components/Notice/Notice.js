f8e7a119329602c0228cc4a025eb20a19aa9677b
JunSangYu <junsang.yu3@gmail.com>
2021-08-11 14:00:10 +0900

    Add(junsang/notice): 공지게시판 글쓰기 버튼 추가

--- src/components/Notice/Notice.js
+++ src/components/Notice/Notice.js
@@ -1,13 +1,18 @@
 import Header from '../Common/Header';
 import NoticeTable from "./NoticeTable";
 import NoticePagination from './NoticePagination';
+import styles from './Notice.module.sass';
 
 function Notice() {
   return (
     <div>
       <Header />
-      <div>
+      <div className={styles.container}>
         <NoticeTable />
+        <div>
+          <p>test</p>
+          <button>✏️ 글쓰기</button>
+        </div>
         <NoticePagination />
       </div>
     </div>
