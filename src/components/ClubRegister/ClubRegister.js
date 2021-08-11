89351c42d7fcb8fb5f0cd4f0636841c1aa818289
Bsfla <baebeomsu77@gmail.com>
2021-08-11 11:22:14 +0900

    Refactor(bumsoo/clubregister): 파일 명 변경 및  불필요한 코드 삭제

--- src/components/ClubRegister/ClubRegister.js
+++ src/components/ClubRegister/ClubRegister.js
@@ -1,43 +1,12 @@
-import React from "react";
-import style from './ClubRegister.module.css';
-
+import style from './ClubRegister.module.css'
+import ClubRegisterForm from './ClubRegisterForm';
 
 const ClubRegister = () => {
-    return (
-        <div className="register">
-            <div className={style.title}><h2>가입 신청</h2></div>
-            <form className={style.userRegister}>
-                <div className={style.inputfield}>
-                    <label>이름</label>
-                    <input type="text" />
-                </div>
-                <div className={style.inputfield}>
-                    <label>학과</label>
-                    <input type="text" />
-                </div>
-                <div className={style.sex}>
-                      <label>성별</label>
-                      <span><input type="radio" />남성</span>
-                      <span><input type="radio" />여성</span>
-                </div>
-                <div className={style.inputfield}>
-                    <label>전화번호</label>
-                    <input type="text" />
-                </div>
-                <div className={style.inputfield}>
-                    <label>나이</label>
-                    <input type="text" />
-                </div>
-                <div className={style.free}>
-                    <label>자유양식</label>
-                    <textarea />
-                </div>
-           </form>
-           <div className={style.btn}>
-               <button>신청</button>
-           </div>
-        </div> 
-    )
+  return (
+      <div className={style.container}>
+          <ClubRegisterForm />
+      </div>
+   ) 
 }
 
 export default ClubRegister;
