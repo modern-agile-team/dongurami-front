e051746ff4417c294e0a4ff30b0692fb651f0b6f
JunSangYu <junsang.yu3@gmail.com>
2021-08-11 16:05:22 +0900

    Add(junsang/comment): 댓글컨테이너, 댓글작성, 댓글 컴포넌트 구현

---
+++ src/components/Common/Comment/Comment.js
@@ -0,0 +1,18 @@
+import styles from './Comment.module.sass';
+
+function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://picsum.photos/500" alt="profile" />
+      <div>
+        <div>
+          <p>닉네임</p>
+          <p>2021.08.11 15:31</p>
+        </div>
+        <div>본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문<br />test<br />test<br />test</div>
+      </div>
+    </div>
+  );
+}
+
+export default Comment;
