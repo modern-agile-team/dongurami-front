18c8e3dddfd53a7472c753ad03812b6a76aa955a
Bsfla <baebeomsu77@gmail.com>
2021-08-11 16:49:38 +0900

    Add(bumsoo/clubschedule): 동아리 일정페이지 1차 레이아웃 구현

---
+++ src/components/Schedule/Schedule.js
@@ -0,0 +1,12 @@
+import styles from "./Schedule.module.sass"
+
+const Schedule = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.schedule}>주요 일정</div>
+            <div className={styles.schedule}>오늘의 일정</div>
+        </div>
+    )
+}
+
+export default Schedule;
