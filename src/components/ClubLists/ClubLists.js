9754e388cfa5e804915cf9ec873dc36979b15b54
Soonki <alstnsrl98@gmail.com>
2021-08-12 17:40:01 +0900

    Add(soonki/promotion): 동아리 홍보 게시판 레이아웃

--- src/components/ClubLists/ClubLists.js
+++ src/components/ClubLists/ClubLists.js
@@ -1,30 +1,49 @@
 import React from "react";
 import Header from "../Common/Header";
 import styles from "./ClubLists.module.sass";
-
+import ClubListContainer from "./ClubListContainer";
+const list = [
+  {
+    title: "우아한 애자일",
+    categories: "IT",
+  },
+  {
+    title: "프리버드",
+    categories: "음악",
+  },
+  {
+    title: "둘리",
+    categories: "맛집",
+  },
+  {
+    title: "학식",
+    categories: "맛집",
+  },
+  {
+    title: "월계국밥",
+    categories: "맛집",
+  },
+  {
+    title: "용궁",
+    categories: "맛집",
+  },
+  {
+    title: "다온",
+    categories: "맛집",
+  },
+  {
+    title: "밥은",
+    categories: "맛집",
+  },
+];
 function ClubLists() {
   return (
     <>
       <Header />
       <div className={styles.body}>
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
+        {list.map((lists, idx) => (
+          <ClubListContainer list={lists} key={idx} />
+        ))}
       </div>
     </>
   );
