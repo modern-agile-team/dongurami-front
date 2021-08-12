ca7b53fdc6eb660b211365953da7e1e4d9350e1c
Bsfla <baebeomsu77@gmail.com>
2021-08-12 13:10:22 +0900

    Add(bumsoo/clublist): 동아리 목록 레이아웃 추가

---
+++ src/pages/clubcontainer.js
@@ -0,0 +1,9 @@
+import ClubListState from "../components/ClubLists/ClubListState";
+
+const clubcontainer = () => {
+    return (
+        <ClubListState />
+    )
+}
+
+export default clubcontainer;
