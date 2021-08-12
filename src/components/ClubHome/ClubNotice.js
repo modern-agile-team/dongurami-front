dea64e5c071fd2572f4e33389f38602c8c6483c5
Bsfla <baebeomsu77@gmail.com>
2021-08-12 13:30:06 +0900

    Add(bumsoo/ClubNotice): 동아리 공지게시판 추가

---
+++ src/components/ClubHome/ClubNotice.js
@@ -0,0 +1,22 @@
+import ClubNoticeTable from "./ClubNoticeTable";
+import ClubNoticeSearch from "./NoticeSearch";
+import ClubNoticePagination from "./NoticePagination";
+import styles from "./ClubNotice.module.sass";
+
+function ClubNotice() {
+  return (
+    <div>
+      <div className={styles.container}>
+        <ClubNoticeTable />
+        <div>
+          <ClubNoticeSearch />
+          <button>✏️ 글쓰기</button>
+        </div>
+        <ClubNoticePagination />
+        <ClubNoticeSearch />
+      </div>
+    </div>
+  );
+}
+
+export default ClubNotice;
