ee03195bc1968e7ea5a0c851fda0401199a2d5c6
Soonki <alstnsrl98@gmail.com>
2021-08-13 11:57:53 +0900

    Modify(soonki/sideBar): 사이드바 레이아웃 조정 및 기능 개선 #72

--- src/components/ClubHome/common/frame.js
+++ src/components/ClubHome/common/frame.js
@@ -1,30 +1,32 @@
-import styles from './frame.module.sass';
-import SideBar from './SideBar';
-import ClubIntro from '../ClubIntro';
-import Activities from '../Activities';
-import Review from '../Review';
-import Calendar from '../Calendar';
-import ClubNotice from '../ClubNotice';
-import { useState } from 'react'
+import styles from "./frame.module.sass";
+import SideBar from "./SideBar";
+import ClubIntro from "../ClubIntro";
+import Activities from "../Activities";
+import Review from "../Review";
+import Calendar from "../Calendar";
+import ClubNotice from "../ClubNotice";
+import { useState } from "react";
 
 const Frame = () => {
-  const [comp, setComp] = useState(1)
+  const [comp, setComp] = useState(1);
 
   const Modal = () => {
-    if (comp === 1) return <ClubIntro />
-    else if (comp === 2) return <Activities />
-    else if (comp === 3) return <Calendar />
-    else if (comp === 4) return <ClubNotice />
-    else if (comp === 5) return <div>자유게시판</div>
-    else if (comp === 6) return <Review />
-  }
+    if (comp === 1) return <ClubIntro />;
+    else if (comp === 2) return <Activities />;
+    else if (comp === 3) return <Calendar />;
+    else if (comp === 4) return <ClubNotice />;
+    else if (comp === 5) return <div>자유게시판</div>;
+    else if (comp === 6) return <Review />;
+  };
 
   return (
-    <div className={styles.wrap}>
-      <SideBar  comp={comp} setComp={setComp}/>
-      <Modal comp={comp} />
-    </div>
-  )
-}
+    <>
+      <SideBar comp={comp} setComp={setComp} />
+      <div className={styles.wrap}>
+        <Modal comp={comp} />
+      </div>
+    </>
+  );
+};
 
 export default Frame;
