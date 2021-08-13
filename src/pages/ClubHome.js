9d60504dc579dc65bd245a9334ef462fb3e7a620
Soonki <alstnsrl98@gmail.com>
2021-08-13 13:24:23 +0900

    Modify(soonki): 동아리 홈 헤더 추가 및 사이드바 고정

--- src/pages/ClubHome.js
+++ src/pages/ClubHome.js
@@ -1,7 +1,13 @@
 import Frame from "../components/ClubHome/common/frame";
+import Header from "../components/Common/Header";
 
 const ClubHome = () => {
-  return <Frame />;
+  return (
+    <>
+      <Header />
+      <Frame />
+    </>
+  );
 };
 
 export default ClubHome;
