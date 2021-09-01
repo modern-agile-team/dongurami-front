fa507e98d5e85dbfe31d35c13730b0a483397aa7
stupidJoon <junsang.yu3@gmail.com>
2021-09-01 11:37:02 +0900

    Add(junsang/board): 게시판 목록 페이지 추가 #101

--- src/components/Notice/NoticeTable.jsx
+++ src/components/Notice/NoticeTable.jsx
@@ -8,18 +8,20 @@ function NoticeTable() {
         <tr>
           <th>번호</th>
           <th>제목</th>
-          <th>글쓴이</th>
-          <th>날짜</th>
+          <th>작성일</th>
+          <th>작성자</th>
+          <th>조회수</th>
         </tr>
       </thead>
       <tbody>
-        {Array.from({ length: 10 }, (_, i) => (
+        {Array.from({ length: 9 }, (_, i) => (
           <Link href="/post" key={i} passHref>
             <tr>
-              <td>{i}</td>
-              <td>제목입니다</td>
-              <td>글쓴2</td>
-              <td>15분전</td>
+              <td>{i + 1}</td>
+              <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+              <td>21-08-27</td>
+              <td>관리자 관리자</td>
+              <td>9999999</td>
             </tr>
           </Link>
         ))}
