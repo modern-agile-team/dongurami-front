2845c0c6d6aa49b16910ffc885722313167e1ce9
seohyunsim <ssh4563@naver.com>
2021-09-02 12:40:45 +0900

    Add(seohyun/find): 아이디/비밀번호 찾기 페이지 생성 밎 ui 구현 후 라우팅

---
+++ src/components/Find/FindPW.js
@@ -0,0 +1,32 @@
+import styles from '../../styles/User/Find/FindPW.module.scss';
+import Link from 'next/link';
+
+export const FindPW = () => {
+    return (
+        <div className={styles.find}>
+            <div className={styles.body}>
+                <h1>비밀번호 찾기</h1>
+                <input className={styles.num} 
+                    type='text' 
+                    placeholder='학번'
+                />
+                <input className={styles.email} 
+                    type='text' 
+                    placeholder='이메일' 
+                />
+            </div> 
+            <div className={styles.findID}>
+                <Link href="/findID" passHref>
+                    <span>아이디 찾기</span>
+                </Link>
+            </div> 
+            <div>
+                <Link href="/" passHref>
+                    <button>비밀번호 찾기</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default FindPW;  
