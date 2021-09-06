62690cb66818af42170f286188c2bf42a00f2dc5
Soonki <alstnsrl98@gmail.com>
2021-09-06 14:10:18 +0900

    Refactor(soonki/manager): 1. 동아리 관리자 페이지 폴더 구조 개선 2. 동아리 원 목록 배열로 변경

--- src/components/ClubHome/common/SideBar.js
+++ src/components/ClubHome/common/SideBar.js
@@ -1,5 +1,5 @@
 import styles from "../../../styles/Club/Home/Common/SideBar.module.scss";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Router from "next/router";
 import { HiMenu, HiPencil } from "react-icons/hi";
 import {
@@ -11,44 +11,39 @@ import {
 } from "react-icons/ai";
 import { MdRateReview } from "react-icons/md";
 
-const SideBar = ({ setComp, comp }) => {
+const SideBar = ({ setComp }) => {
   const [isOpen, setOpen] = useState(true);
+  const toggle = () => setOpen((isOpen) => !isOpen);
   const iconSize = 25;
-  const toggle = () => setOpen(!isOpen);
-
   const movePage = () => {
     Router.push("/manager");
   };
-
-  if (typeof window !== "undefined") {
-    window.scrollTo(0, 0);
-  }
 
   return (
     <div className={styles.sideBar}>
       <HiMenu id={styles.hamb} onClick={() => toggle()} size="30" />
       <div className={styles.menu} id={isOpen ? styles.show : styles.hide}>
-        <div id={comp === 1 ? styles.now : 0} onClick={() => setComp(1)}>
+        <div onClick={() => setComp(1)}>
           <AiOutlineHome size={iconSize} />
           <span>우아한 애자일</span>
         </div>
-        <div id={comp === 2 ? styles.now : 0} onClick={() => setComp(2)}>
+        <div onClick={() => setComp(2)}>
           <AiOutlineNotification size={iconSize} />
           <span>공지 사항</span>
         </div>
-        <div id={comp === 3 ? styles.now : 0} onClick={() => setComp(3)}>
+        <div onClick={() => setComp(3)}>
           <AiOutlinePicLeft size={iconSize} />
           <span>활동내용</span>
         </div>
-        <div id={comp === 4 ? styles.now : 0} onClick={() => setComp(4)}>
+        <div onClick={() => setComp(4)}>
           <AiOutlineSchedule size={iconSize} />
           <span>일정</span>
         </div>
-        <div id={comp === 5 ? styles.now : 0} onClick={() => setComp(5)}>
+        <div onClick={() => setComp(5)}>
           <MdRateReview size={iconSize} />
           <span>동아리 후기</span>
         </div>
-        <div id={comp === 6 ? styles.now : 0} onClick={() => setComp(6)}>
+        <div onClick={() => setComp(6)}>
           <HiPencil size={iconSize} />
           <span>동아리 지원하기</span>
         </div>
