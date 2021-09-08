21dcdfece9b4474c2bacfcdd472298f836eb2bf9
Min <alstnsrl98@gmail.com>
2021-09-08 15:03:15 +0900

    Add(soonki/review): 동아리 리뷰에 동아리 로고와 테두리 추가

--- src/components/ClubHome/Review/ReviewList.js
+++ src/components/ClubHome/Review/ReviewList.js
@@ -7,14 +7,25 @@ export const ReviewList = ({ rate, desc, date }) => {
   console.log(stars);
   return (
     <div className={styles.list}>
-      <div className={styles.star}>
-        {stars.map((el) => {
-          return <AiFillStar key={el[0]} />;
-        })}
+      <div className={styles.header}>
+        <div className={styles.club}>
+          <img
+            src="https://lovelyoch.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F65bcaa80-1b5b-4662-82c2-41d689605df2%2F%EC%9A%B0%EC%95%84%ED%95%9C_%EC%95%A0%EC%9E%90%EC%9D%BC_%EB%A1%9C%EA%B3%A0.png?table=block&id=c6f4102e-005c-45a7-bc48-27ecaacc7a0d&spaceId=69eb8ea8-3d04-47ec-8bb7-004e8aa31f9e&width=250&userId=&cache=v2"
+            alt="우아한 애자일"
+          />
+          <div id={styles.date}>
+            <span>우아한 애자일</span>
+            <p>{date}</p>
+          </div>
+        </div>
+        <div className={styles.star}>
+          {stars.map((el) => {
+            return <AiFillStar key={el[0]} />;
+          })}
+        </div>
       </div>
       <div className={styles.review}>
         <p>{desc}</p>
-        <p>{date}</p>
       </div>
     </div>
   );
