9053ad7f8ed44c4cba04543459e065d927c5f265
Min <alstnsrl98@gmail.com>
2021-09-08 22:13:42 +0900

    Add(soonki/apply): 동아리 지원하기 추가 질문 추가 삭제 기능 구현

--- src/components/ClubHome/Apply/Additional.js
+++ src/components/ClubHome/Apply/Additional.js
@@ -3,36 +3,7 @@ import styles from "../../../styles/Club/Home/Apply/Additional.module.scss";
 import { HiPencil } from "react-icons/hi";
 import { FaTrashAlt } from "react-icons/fa";
 
-const list = [
-  {
-    question: "지원 동기",
-  },
-  {
-    question: "다룰 줄 아는 프로그래밍 언어는?",
-  },
-  {
-    question: "하고 싶은 프로젝트",
-  },
-];
-
-const Additional = () => {
-  const a = new Array(list.length).fill(false);
-  const [isUpdate, setIsUpdate] = useState(a);
-
-  const onUpdate = (i) => {
-    const b = isUpdate.map((el, index) => {
-      return i === index ? !el : el;
-    });
-    setIsUpdate(b);
-  };
-
-  const onRemove = (i) => {
-    const b = list.filter((el, index) => {
-      return i !== index;
-    });
-    console.log(b);
-  };
-
+const Additional = ({ list, onRemove, onUpdate, isUpdate }) => {
   return (
     <div className={styles.additional}>
       <ul>
