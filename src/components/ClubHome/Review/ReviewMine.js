a749da4f1206b3a18892c0965322dc94149ef1e7
Min <alstnsrl98@gmail.com>
2021-09-08 15:09:25 +0900

    Modify(soonki/review): 나의 후기 수정 삭제 아이콘 위치 조정

--- src/components/ClubHome/Review/ReviewMine.js
+++ src/components/ClubHome/Review/ReviewMine.js
@@ -13,13 +13,9 @@ export const ReviewMine = () => {
             src="https://lovelyoch.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F65bcaa80-1b5b-4662-82c2-41d689605df2%2F%EC%9A%B0%EC%95%84%ED%95%9C_%EC%95%A0%EC%9E%90%EC%9D%BC_%EB%A1%9C%EA%B3%A0.png?table=block&id=c6f4102e-005c-45a7-bc48-27ecaacc7a0d&spaceId=69eb8ea8-3d04-47ec-8bb7-004e8aa31f9e&width=250&userId=&cache=v2"
             alt="우아한 애자일"
           />
-          <div>
+          <div id={styles.date}>
             <span>우아한 애자일</span>
             <p>2021-08-30</p>
-          </div>
-          <div className={styles.update}>
-            <HiPencil />
-            <FaTrashAlt />
           </div>
         </div>
         <div className={styles.star}>
@@ -28,6 +24,10 @@ export const ReviewMine = () => {
           <AiFillStar />
           <AiFillStar />
           <AiFillStar />
+          <div className={styles.update}>
+            <HiPencil />
+            <FaTrashAlt />
+          </div>
         </div>
       </div>
       <div className={styles.review}>
