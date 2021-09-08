665136bbaa45830fc7892ab2343b482ff7710325
Min <alstnsrl98@gmail.com>
2021-09-08 17:55:52 +0900

    Add(soonki/manage): 가입 승인 대기 리스트 토글 버튼 커서 pointer

--- src/components/ClubHome/Manager/ApproveHeader.js
+++ src/components/ClubHome/Manager/ApproveHeader.js
@@ -3,14 +3,15 @@ import { BsToggleOn, BsToggleOff } from "react-icons/bs";
 
 const ApproveHeader = ({ answers, onClick, listOpen }) => {
   const iconSize = 25;
+  const cursor = "pointer";
   return (
     <div>
       <h1>가입 승인 대기</h1>
       <h3>{answers.length}명</h3>
       {listOpen ? (
-        <BsToggleOn onClick={onClick} size={iconSize} />
+        <BsToggleOn cursor={cursor} onClick={onClick} size={iconSize} />
       ) : (
-        <BsToggleOff onClick={onClick} size={iconSize} />
+        <BsToggleOff cursor={cursor} onClick={onClick} size={iconSize} />
       )}
       <hr />
     </div>
