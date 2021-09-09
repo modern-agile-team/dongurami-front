e8ffa35c1b549475e6d0528dd91fe9cef8ada8c5
parkhyunwoo <pho9902@naver.com>
2021-09-09 12:00:16 +0900

    Modify(hyunwoo/calendar): DailyModal 팝업형태로 수정, caledar scss 수정

--- src/components/ClubHome/Daily/Calendar.js
+++ src/components/ClubHome/Daily/Calendar.js
@@ -12,6 +12,7 @@ const Calendar =()=>{
   const info = {
     startDate: "2021-09-07",
     endDate: "2021-09-09",
+    term: 3,
     title: '살려주세요',
     color: '#44444'
   }
@@ -25,6 +26,7 @@ const Calendar =()=>{
   const calendarArr=()=>{
     let result = [];
     let week = firstWeek;
+    const weekDays = ["일", "월", "화", "수", "목", "금", "토"]
     for (week; week <= lastWeek; week++) {
       result = result.concat(
         <tr key={week}>
@@ -35,19 +37,20 @@ const Calendar =()=>{
                 if(moment().format('YYYYMMDD') === days.format('YYYYMMDD')){
                   return(
                       <td className={styles.dayblock} key={index}>
-                        <span className={styles.today} onClick={() => setDate(days.format('YYYYMMDD'))}>{days.format('D')}</span>
+                        <span className={styles.today} onClick={() => setDate(days.format('YYYYMMDD'))}>{days.format('D')}{weekDays[days.day()]}</span>
+                        {/* <span>{weekDays[days.day()]}</span> */}
                       </td>
                   );
                 }else if(days.format('MM') !== today.format('MM')){
                   return(
                       <td key={index} className={styles.dayblock}>
-                        <span onClick={() => setDate(days.format('YYYYMMDD'))} className={styles.otherday}>{days.format('D')}</span>
+                        <span onClick={() => setDate(days.format('YYYYMMDD'))} className={styles.otherday}>{days.format('D')}{weekDays[days.day()]}</span>
                       </td>
                   );
                 }else{
                   return(
                       <td key={index} className={styles.dayblock}>
-                        <span className={styles.monthdays} id={days.format('YYYYMMDD')} onClick={() => setDate(days.format('YYYYMMDD'))}>{days.format('D')}</span>
+                        <span className={styles.monthdays} id={days.format('YYYYMMDD')} onClick={() => setDate(days.format('YYYYMMDD'))}>{days.format('D')}{weekDays[days.day()]}</span>
                       </td>
                   );
                 }
@@ -58,34 +61,28 @@ const Calendar =()=>{
     return result;
   }
    return (
-    <div className={styles.wrap}>
-      <Schedule />
-        <div className={styles.control}>
-          <button className={styles.lastmonthBtn} onClick={()=>{ setMoment(getMoment.clone().subtract(1, 'month')) }} >이전달</button>
-          <span>{today.format('YYYY 년 MM 월')}</span>
-          <button className={styles.nextmonthBtn} onClick={()=>{ setMoment(getMoment.clone().add(1, 'month')) }} >다음달</button>
-        </div>
-        <button className={styles.addBtn} onClick={() => setPop(1)}>일정 추가하기</button>
-        <div className={styles.days}>
-          <h2>일</h2>
-          <h2>월</h2>
-          <h2>화</h2>
-          <h2>수</h2>
-          <h2>목</h2>
-          <h2>금</h2>
-          <h2>토</h2>
-        </div>
-        <table className={styles.calendar}>
-          <tbody>
-            {calendarArr()}
-          </tbody>
-        </table>
-        <DailyModal 
-          setPop={setPop}
-          today={today.format('YYYY-MM-DD')}
-          pop={pop}
-        />
-    </div>
+     <>
+      <div className={styles.wrap}>
+        <Schedule className={styles.scheduleComp}/>
+          <div className={styles.control}>
+            <span className={styles.lastmonth} onClick={()=>{ setMoment(getMoment.clone().subtract(1, 'month')) }} >&lt;</span>
+            <span className={styles.thisMonth}>{today.format('YYYY년 MM월')}</span>
+            <span className={styles.nextmonth} onClick={()=>{ setMoment(getMoment.clone().add(1, 'month')) }} >&gt;</span>
+          </div>
+          <div className={styles.add}>
+            <span className={styles.addBtn} onClick={() => setPop(1)}>일정 추가하기</span>
+          </div>
+          <table className={styles.calendar}>
+            <tbody>
+              {calendarArr()}
+            </tbody>
+          </table>  
+      </div>
+      <DailyModal 
+      setPop={setPop}
+      today={today.format('YYYY-MM-DD')}
+      pop={pop} />
+    </>
   );
 }
 export default Calendar;
--- src/components/ClubHome/Daily/DailyModal.js
+++ src/components/ClubHome/Daily/DailyModal.js
@@ -1,5 +1,6 @@
 import styles from '../../../styles/Club/Home/Schedule/DailyModal.module.scss';
 import { useState, useRef } from 'react';
+import { MdClose } from "react-icons/md";
 
 const DailyModal = ({ setPop, pop, today }) => {
   const [startDate, setStartDate] = useState(today);
@@ -9,13 +10,12 @@ const DailyModal = ({ setPop, pop, today }) => {
 
   if (pop === 0) return null;
   return (
-    <>
-      <div className={styles.wrap}> 
+    <div className={styles.wrap}>
+      <div className={styles.modal}> 
         <div className={styles.header}>
           <h3>일정 작성</h3> 
-          <button onClick={() => setPop(0)}>X</button>
         </div>
-
+        <MdClose className={styles.closeBtn} onClick={() => setPop(0)} />
         <div className={styles.body}>
           <p>시작하는 날짜</p>
           <input id='startDate' type='date' ref={startInput} onChange={() => setStartDate(startInput.current.value)}/>
@@ -23,14 +23,14 @@ const DailyModal = ({ setPop, pop, today }) => {
           <input id='endDate' type='date' ref={endInput} onChange={() => setEndDate(endInput.current.value)}/> <br /> <br />
           {startDate} ~ {endDate} <br />
           <p>일정 제목</p>
-          <input type='text' placeholder='일정 제목을 입력하세요' /><br />
+          <input type='text' placeholder='일정 제목을 입력하세요' className={styles.titleInput}/><br />
           <p>일정 색상</p>
-          <input type='color' />
+          <input type='color' className={styles.choiceColor}/>
         </div>
       </div>
-      <button>추가하기</button>
+      <button className={styles.addBtn}>추가하기</button>
       {/* onClick = {}fetch로 기간 제목 색상 보내서 저장하기 */}
-    </>
+    </div>
   )
 }
 
