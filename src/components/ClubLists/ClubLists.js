2d54b7cb9ce6b303861fa5936c471b08e5e04fb8
Bsfla <baebeomsu77@gmail.com>
2021-09-13 16:33:38 +0900

    Add(bumsoo/clublist): 동아리 목록 조회  api적용

--- src/components/ClubLists/ClubLists.js
+++ src/components/ClubLists/ClubLists.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import styles from "../../styles/Club/Lists/ClubLists.module.scss";
 import ClubListContainer from "./ClubListContainer";
+import axios from "axios";
 const list = [
   {
     title: "우아한 애자일",
@@ -43,17 +44,33 @@ const list = [
     img: 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg'
   },
 ];
+
+
 const ClubList = () => {
+  const [clubData, setClubData] = useState([]);
+  const img = 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg';
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const response = await axios.get('http://3.36.72.145:8080/api/club/list');
+        setClubData(response.data.result);
+      } catch(e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+  }, []);
+  
   return (
     <div className={styles.container}>
       <div className={styles.activities}>
-        {list.map((el) => {
+        {clubData.map((el) => {
           return (
             <ClubListContainer
-              img={el.img}
-              title={el.title}
-              categories={el.categories}
-              key={el.key}
+              img={img}
+              title={el.name}
+              categories={el.category}
+              key={el.no}
             />
           );
         })}
