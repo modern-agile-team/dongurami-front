33dd35ed0738a94cbc1e63c1e8efe46cf176cdb0
Soonki <alstnsrl98@gmail.com>
2021-09-14 14:42:44 +0900

    Modify(soonki/clubHome): 동아리 홈 fetch axios로 변경

--- src/components/ClubHome/Intro/ClubIntro.js
+++ src/components/ClubHome/Intro/ClubIntro.js
@@ -2,6 +2,7 @@ import styles from "../../../styles/Club/Home/Intro/ClubIntro.module.scss";
 import ClubInfo from "./ClubInfo";
 import Desc from "./Desc";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ClubIntro = () => {
   const [info, setInfo] = useState([]);
@@ -16,40 +17,48 @@ const ClubIntro = () => {
     setIntroDesc(e.target.value);
   };
 
-  const onDescSubnmit = () => {
-    fetch("http://3.36.72.145:8080/api/club/home/1", {
+  const onDescSubnmit = async () => {
+    const options = {
       method: "PUT",
       headers: {
         "Content-type": "application/json; charset=utf-8",
         "x-auth-token":
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3QxIiwibmFtZSI6InRlc3QxIiwiY2x1Yk51bSI6IlsxXSJ9.1u6k5cJuaUlZj14CJJZiI8guHnlZXf1uuU6vZjl9jNk",
       },
-      body: JSON.stringify({
+      data: {
         logoUrl: info[0].logoUrl,
         fileId: info[0].fileId,
         introduce: introDesc,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+      },
+    };
+    await axios("http://3.36.72.145:8080/api/club/home/1", options)
+      .then((res) =>
+        res.data
+          ? alert("글이 수정되었습니다.")
+          : alert("글 수정에 실패했습니다")
+      )
+      .catch((err) => alert(err.response.data.msg));
+
     setDescUpdate(!descUpdate);
   };
 
   const getClubData = async () => {
-    await fetch("http://3.36.72.145:8080/api/club/home/1", {
+    const options = {
       headers: {
         "Content-type": "application/json; charset=utf-8",
         "x-auth-token":
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3QxIiwibmFtZSI6InRlc3QxIiwiY2x1Yk51bSI6IlsxXSJ9.1u6k5cJuaUlZj14CJJZiI8guHnlZXf1uuU6vZjl9jNk",
       },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setInfo(data.result);
-        setIntroDesc(data.result[0].introduce);
-      });
+    };
+    await axios
+      .get("http://3.36.72.145:8080/api/club/home/1", options)
+      .then((res) => {
+        setInfo(res.data.result);
+        setIntroDesc(res.data.result[0].introduce);
+      })
+      .catch((err) => alert(err.response.data.msg));
   };
+
   useEffect(() => {
     getClubData();
   }, []);
