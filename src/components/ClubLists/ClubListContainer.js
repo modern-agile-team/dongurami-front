015d2920d0c10c5d42dcbc332f830533a8cba6ac
Bsfla <baebeomsu77@gmail.com>
2021-09-14 14:03:55 +0900

    Modify(bumsoo/clublist): 파일 이름 변경

--- src/components/ClubLists/ClubListContainer.js
+++ src/components/ClubLists/ClubListContainer.js
@@ -1,18 +1,48 @@
-import React, { useState } from 'react';
-import styles from '../../styles/Club/Lists/ClubListContainer.module.scss';
-import { useRouter } from "next/router";
+import React, { useState, useEffect} from "react";
+import styles from "../../styles/Club/Lists/ClubLists.module.scss";
+import ClubList from "./ClubList";
+import axios from "axios";
 
-const ClubListContainer = ({img, categories, title}) => {
-    const router = useRouter();   
-    return (
-        <div className={styles.container}>
-           <img src={img} alt="동아리" onClick={() => router.push('/ClubHome')} />
-           <div>
-             <p id={styles.desc}>{title}</p>
-             <p>#{categories}</p>
-           </div>
-        </div>
-    )
-}
+const ClubList = () => {
+  const [clubData, setClubData] = useState([]);
+  const img = 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg';
+
+  const onClick = () => {
+
+  }
+  
+  useEffect(() => {
+    const getData = async() => {
+      try {
+        const response = await axios.get('http://3.36.72.145:8080/api/club/list');
+        setClubData(response.data.result);
+        
+      } catch(e) {
+        console.log(e);
+      }
+    }
+    getData();
+
+  }, []);
+  
+  return (
+    <div className={styles.container}>
+      <div className={styles.activities}>
+        {clubData.map((el) => {
+          return (
+            <ClubList
+              img={img}
+              title={el.name}
+              categories={el.category}
+              onClick={onClick}
+              key={el.no}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default ClubListContainer;
+
