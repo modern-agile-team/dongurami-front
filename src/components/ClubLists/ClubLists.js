3fb0f926d156a5243a8c2cd784594b951aa0863d
Bsfla <baebeomsu77@gmail.com>
2021-09-14 12:27:38 +0900

    Add(bumsoo/clublist): 동아리 목록 api적용

--- src/components/ClubLists/ClubLists.js
+++ src/components/ClubLists/ClubLists.js
@@ -2,58 +2,17 @@ import React, { useState, useEffect} from "react";
 import styles from "../../styles/Club/Lists/ClubLists.module.scss";
 import ClubListContainer from "./ClubListContainer";
 import axios from "axios";
-const list = [
-  {
-    title: "우아한 애자일",
-    categories: "IT",
-    img: 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg'
-  },
-  {
-    title: "프리버드",
-    categories: "음악",
-    img:  'https://i.pinimg.com/236x/82/13/43/82134367f8d18218a5175b9778ec97e4.jpg'
-  },
-  {
-    title: "소리상공",
-    categories: "음악",
-    img: 'https://i.pinimg.com/236x/06/19/93/061993aa61c91040f93157bfc6f373f8.jpg'
-  },
-  {
-    title: "우아한 애자일",
-    categories: "IT",
-    img: 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg'
-  },
-  {
-    title: "우아한 애자일",
-    categories: "IT",
-    img: 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg'
-  },
-  {
-    title: "우아한 애자일",
-    categories: "IT",
-    img: 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg'
-  },
-  {
-    title: "우아한 애자일",
-    categories: "IT",
-    img: 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg'
-  },
-  {
-    title: "우아한 애자일",
-    categories: "IT",
-    img: 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg'
-  },
-];
-
 
 const ClubList = () => {
   const [clubData, setClubData] = useState([]);
   const img = 'https://i.pinimg.com/236x/5b/4f/3f/5b4f3f801c99430ef0189e0fd8bc5855.jpg';
+  
   useEffect(() => {
     const fetchData = async() => {
       try {
         const response = await axios.get('http://3.36.72.145:8080/api/club/list');
         setClubData(response.data.result);
+        console.log(clubData);
       } catch(e) {
         console.log(e);
       }
--- src/components/Promotion/PromotionContainer.js
+++ src/components/Promotion/PromotionContainer.js
@@ -1,17 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../../styles/Board/Promotion/PromotionContainer.module.scss';
-import { data } from './data';
-import Link from 'next/link';
 import Header from '../Common/Header';
 import TypeSearch from './TypeSearch';
 import { BsPencil } from 'react-icons/bs';
 import Modal from './Modal';
 import Promotion from './Promotion';
+import Link from 'next/dist/client/link';
+import axios from 'axios';
 
 const PromotionContainer = () => {
     const [openModal, setOpenModal] = useState(false);
     const [value, setValue] = useState('');
+    const [boarddata, setBoardData ] = useState([]);
+    const img = 'https://i.pinimg.com/236x/df/ef/48/dfef48b50816f9d55767a0260798f0d2.jpg'
     
+    let preitem = 0;
+    let item = 8;
+    
+    useEffect(() => {
+       const fetchData = async() => {
+            
+            try {
+                await axios.get('http://3.36.72.145:8080/api/board/wholeNotice/inDate/DESC')
+                .then((response) => {
+                    let result = response.data.boards.slice(preitem, item)
+                    console.log(result);
+                })
+            } catch(e) {
+                console.log(e);
+            }
+        } 
+        fetchData();
+    },[]);
+      
+    
+
+
     return (
      <>
       <Header />
@@ -23,8 +47,8 @@ const PromotionContainer = () => {
             </button>
         </Link>
        <div className={styles.section}>
-            {data.map(el => (
-                <Promotion key={el.key} username={el.user_name} img={el.img} setOpenModal={setOpenModal} setValue={ setValue } />
+            {boarddata.map(el => (
+                <Promotion key={el.no} date={el.inDate} clubName={el.clubName} img={img} setOpenModal={setOpenModal} setValue={ setValue } />
             ))}
            
         </div>
