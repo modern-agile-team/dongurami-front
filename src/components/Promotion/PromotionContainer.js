5040017817f2e60eb37b941044e905aa847761fa
Bsfla <baebeomsu77@gmail.com>
2021-09-14 15:16:30 +0900

    Fix(bumsoo/promo): yarn lock파일 삭제

--- src/components/Promotion/PromotionContainer.js
+++ src/components/Promotion/PromotionContainer.js
@@ -1,74 +1,86 @@
-import { useEffect, useState } from 'react';
-import styles from '../../styles/Board/Promotion/PromotionContainer.module.scss';
-import Header from '../Common/Header';
-import TypeSearch from './TypeSearch';
-import { BsPencil } from 'react-icons/bs';
-import Modal from './Modal';
-import Promotion from './Promotion';
-import Link from 'next/dist/client/link';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import styles from "../../styles/Board/Promotion/PromotionContainer.module.scss";
+import Header from "../Common/Header";
+import TypeSearch from "./TypeSearch";
+import { BsPencil } from "react-icons/bs";
+import Modal from "./Modal";
+import Promotion from "./Promotion";
+import Link from "next/dist/client/link";
+import axios from "axios";
 
 const PromotionContainer = () => {
-    const [openModal, setOpenModal] = useState(false);
-    const [value, setValue] = useState('');
-    const [boarddata, setBoardData ] = useState([]);
-    const img = 'https://i.pinimg.com/236x/df/ef/48/dfef48b50816f9d55767a0260798f0d2.jpg'
-    
-    let preitem = 0;
-    let item = 8;
+  const [openModal, setOpenModal] = useState(false);
+  const [value, setValue] = useState("");
+  const [boarddata, setBoardData] = useState([]);
+  const img =
+    "https://i.pinimg.com/236x/df/ef/48/dfef48b50816f9d55767a0260798f0d2.jpg";
 
-    const getData = async() => {
-        try {
-            await axios.get('http://3.36.72.145:8080/api/board/wholeNotice/inDate/DESC')
-            .then((response) => {
-                const result = response.data.boards.slice(preitem, item);
-                const extraData = boarddata.concat(result);
+  let preitem = 0;
+  let item = 8;
 
-                setBoardData(prev => prev.concat(extraData));
-            })
-        } catch(e) {
-            console.log(e);
-        }
+  const getData = async () => {
+    try {
+      await axios
+        .get("http://3.36.72.145:8080/api/board/wholeNotice/inDate/DESC")
+        .then((response) => {
+          const result = response.data.boards.slice(preitem, item);
+          const extraData = boarddata.concat(result);
+
+          setBoardData((prev) => prev.concat(extraData));
+        });
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const infiniteScroll  = () => {
-        let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
-        let scrollTop =  Math.max(document.documentElement.scrollTop, document.body.scrollTop);
-        let clientHeight = document.documentElement.clientHeight;
+  const infiniteScroll = () => {
+    let scrollHeight = Math.max(
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight
+    );
+    let scrollTop = Math.max(
+      document.documentElement.scrollTop,
+      document.body.scrollTop
+    );
+    let clientHeight = document.documentElement.clientHeight;
 
-        if (scrollTop + clientHeight >= scrollHeight) {
-             preitem = item;
-             item += 8;
-             getData();
-        }
-    } 
-    
-    
-    useEffect(() => {
-        getData();
-        window.addEventListener('scroll', infiniteScroll);
+    if (scrollTop + clientHeight >= scrollHeight) {
+      preitem = item;
+      item += 8;
+      getData();
+    }
+  };
 
-     },[]);
+  useEffect(() => {
+    getData();
+    window.addEventListener("scroll", infiniteScroll);
+  }, []);
 
-     return (
-     <>
+  return (
+    <>
       <Header />
-        <TypeSearch />
-        <Link href="/write" passHref>
-            <button className={styles.writeBtn}>
-                <BsPencil />
-                글쓰기
-            </button>
-        </Link>
-       <div className={styles.section}>
-            {boarddata.map(el => (
-                <Promotion key={el.no} date={el.inDate} clubName={el.clubName} img={img} setOpenModal={setOpenModal} setValue={ setValue } />
-            ))}
-           
-        </div>
-        {openModal && <Modal value={value} setOpenModal={setOpenModal} />}
-    </>  
-    )
-}
+      <TypeSearch />
+      <Link href="/write" passHref>
+        <button className={styles.writeBtn}>
+          <BsPencil />
+          글쓰기
+        </button>
+      </Link>
+      <div className={styles.section}>
+        {boarddata.map((el) => (
+          <Promotion
+            key={el.no}
+            date={el.inDate}
+            clubName={el.clubName}
+            img={img}
+            setOpenModal={setOpenModal}
+            setValue={setValue}
+          />
+        ))}
+      </div>
+      {openModal && <Modal value={value} setOpenModal={setOpenModal} />}
+    </>
+  );
+};
 
 export default PromotionContainer;
--- src/components/Promotion/TypeSearch.js
+++ src/components/Promotion/TypeSearch.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import styles from '../../styles/Board/Promotion/typeSearch.module.scss';
+import React from "react";
+import styles from "../../styles/Board/Promotion/typeSearch.module.scss";
 import { FaSearch } from "react-icons/fa";
-const TypeSearch = ({onCategorySearch}) => {
-    
-    return (
-        <div className={styles.container}>
-           <ul className={styles.tagList} onClick={(event) => onCategorySearch(event.target.getAttribute('name'))}>
-               <li name='IT'>#IT</li>
-               <li name='음악'>#음악</li>
-               <li name='친목'>#친목</li>
-               <li name='게임'>#게임</li>
-               <li name='운동'>#운동</li>
-           </ul>
-           <div className={styles.searchContainer}>
-               <div className={styles.searchElement}>
-                  <input type="text" placeholder="Search" />
-                  <FaSearch />
-                </div>
-            </div>
+const TypeSearch = ({ onCategorySearch }) => {
+  return (
+    <div className={styles.container}>
+      <ul
+        className={styles.tagList}
+        onClick={(event) => onCategorySearch(event.target.getAttribute("name"))}
+      >
+        <li name="IT">#IT</li>
+        <li name="음악">#음악</li>
+        <li name="친목">#친목</li>
+        <li name="게임">#게임</li>
+        <li name="운동">#운동</li>
+      </ul>
+      <div className={styles.searchContainer}>
+        <div className={styles.searchElement}>
+          <input type="text" placeholder="동아리명을 검색해주세요" />
+          <FaSearch />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default TypeSearch;
