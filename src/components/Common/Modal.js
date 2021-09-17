a238be411c57a874466f00b6f03a93a73fbedc76
JunSangYu <junsang.yu3@gmail.com>
2021-09-17 15:28:00 +0900

    Modify(junsang/board): 모달 수정

--- src/components/Common/Modal.js
+++ src/components/Common/Modal.js
@@ -1,8 +1,20 @@
+import { useRef } from 'react';
+import { MdClose } from 'react-icons/md';
 import styles from 'styles/Common/Modal.module.scss';
 
-function Modal({ children, show }) {
+function Modal({ children, show, onClose }) {
+  const modalContainer = useRef();
+
+  const onClick = (e) => {
+    if (e.target !== modalContainer.current) return;
+    onClose();
+  }
+
   return (
-    <div className={(show) ? styles.open : styles.close}>
+    <div className={(show) ? styles.open : styles.close} ref={modalContainer} onClick={onClick}>
+      <button onClick={onClose}>
+        <MdClose />
+      </button>
       <div>
         {children}
       </div>
