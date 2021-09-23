ca7818bc854586bd41f32ae3589b4ac3c6496fb2
Min <alstnsrl98@gmail.com>
2021-09-24 00:55:53 +0900

    Refactor(soonki/manager): api 요청 등 주요 코드들 컨테이너 파일(Manger.js)로 이동

--- src/components/ClubHome/Manager/Members.js
+++ src/components/ClubHome/Manager/Members.js
@@ -1,39 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "../../../styles/Club/Home/Manager/Manager.module.scss";
 import MembersMeber from "./MembersMember";
 import MembersHeader from "./MembersHeader";
 import MembersPreface from "./MembersPreface";
-import axios from "axios";
 
-export const Members = () => {
-  const [members, setMembers] = useState([]);
-  const [leader, setLeader] = useState("");
-  let jwtTocken = "";
-
-  if (typeof window !== "undefined") {
-    jwtTocken = localStorage.getItem("jwt");
-  }
-
-  const getMembersData = async () => {
-    const options = {
-      headers: {
-        "Content-type": "application/json; charset=utf-8",
-        "x-auth-token": jwtTocken,
-      },
-    };
-    await axios
-      .get("http://3.36.72.145:8080/api/club/admin-option/1", options)
-      .then((res) => {
-        setLeader(res.data.clubAdminOption.leader);
-        setMembers(res.data.clubAdminOption.memberAndAuthList);
-      })
-      .catch((err) => console.log(err.response.data.msg[0]));
-  };
-
-  useEffect(() => {
-    getMembersData();
-  }, []);
-
+export const Members = ({ members, leader }) => {
   return (
     <div>
       <MembersHeader members={members.length} />
