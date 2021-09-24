b450bfd79bb5c3e30092510f9652652c98c903b7
stupidJoon <junsang.yu3@gmail.com>
2021-09-24 16:19:21 +0900

    Refactor(junsang/board): 컴포넌트 리팩토링

--- src/pages/free/index.js
+++ src/pages/free/index.js
@@ -1,7 +1,13 @@
+import axios from 'axios';
 import Board from "../../components/Board/Board";
 
+const getPosts = async (order) => {
+  const response = await axios.get(`http://3.36.72.145:8080/api/board/free/${order.split(' ').join('/')}`);
+  return response;
+}
+
 function free() {
-  return <Board category="free"/>;
+  return <Board category="free" getPosts={getPosts}/>;
 }
 
 export default free;
