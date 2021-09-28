c82910f3b836bd6ee9935a622f78578d11b48c6d
parkhyunwoo <pho9902@naver.com>
2021-09-28 11:27:07 +0900

    Refactor(hyunwoo/myPage): 쓰지않는 import 구문 삭제 myPage 라우팅 부분 next/Router 로 교체

--- src/components/ClubHome/Daily/Schedule.js
+++ src/components/ClubHome/Daily/Schedule.js
@@ -2,7 +2,9 @@ import styles from "../../../styles/Club/Home/Schedule/Schedule.module.scss";
 import Router from "next/router";
 
 const Schedule = () => {
-  const goMypage = () => {Router.push("/myPage")}
+  const goMypage = () => {
+    Router.push("/myPage");
+  };
 
   return (
     <div className={styles.container}>
@@ -10,13 +12,13 @@ const Schedule = () => {
         <h4>주요 일정</h4>
         <div>
           <p>추석</p>
-          <p>9월 20일 ~ 9월 22일</p>  
+          <p>9월 20일 ~ 9월 22일</p>
         </div>
       </div>
       <div className={styles.schedule}>
         <h4>오늘의 일정</h4>
         <p>일정이 없습니다</p>
-        <span onClick={goMypage}>박현우</span>  
+        <span onClick={goMypage}>박현우</span>
       </div>
     </div>
   );
