ab7dbd79b7961c86ee1c1a7742aa70174f72db10
seohyunsim <ssh4563@naver.com>
2021-09-28 15:18:20 +0900

    Conflict(seohyun/Header): notice page  header import 수정

--- src/pages/notice/index.js
+++ src/pages/notice/index.js
@@ -1,12 +1,14 @@
 import Board from "../../components/Board/Board";
 import Footer from "../../components/Common/Footer";
-import axios from 'axios';
-import Header from "components/Common/Header";
+import axios from "axios";
+import Header from "components/Common/Header/Header";
 
 const getPosts = async (order) => {
-  const response = await axios.get(`http://3.36.72.145:8080/api/board/notice/${order.split(' ').join('/')}`);
+  const response = await axios.get(
+    `http://3.36.72.145:8080/api/board/notice/${order.split(" ").join("/")}`
+  );
   return response;
-}
+};
 
 function Notice() {
   return (
