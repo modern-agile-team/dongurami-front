05828429c3e597aad87b949250eb0ba148c8eede
Bsfla <baebeomsu77@gmail.com>
2021-09-29 20:52:29 +0900

    Add(bumsoo/promotion): 댓글 컴포넌트 추가

--- src/components/Promotion/Comment/PromotionCommentContainer.js
+++ src/components/Promotion/Comment/PromotionCommentContainer.js
@@ -1,6 +1,7 @@
 import style from "../../../styles/Board/Promotion/PromotionCommentContainer.module.scss";
 import AddComment from "./AddComment";
 import React, { useState } from "react";
+import Comment from "./Comment";
 
 const PromotionCommentContainer = ({ comments, postId }) => {
   const [addReplyID, setAddReplyID] = useState();
@@ -10,6 +11,12 @@ const PromotionCommentContainer = ({ comments, postId }) => {
       <p>댓글 {comments.length}</p>
       <hr />
       <div className={style.container}>
+        {comments &&
+          comments.map((comment, index) => (
+            <>
+              <Comment key={index} comment={comment} postId={postId} />
+            </>
+          ))}
         <AddComment comments={comments} postId={postId} />
       </div>
     </>
