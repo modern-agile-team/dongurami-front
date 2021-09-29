d2125024c963227dc792742609c805580bd4dc6f
parkhyunwoo <pho9902@naver.com>
2021-09-29 17:10:17 +0900

    conflict(hyunwoo/globalscss): 충돌 수정

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,6 +1,6 @@
 import Profile from "components/Profile/Profile";
 import Footer from "components/Common/Footer";
-import Header from "components/Common/Header";
+import Header from "components/Common/Header/Header";
 
 export default function profile() {
   return (
