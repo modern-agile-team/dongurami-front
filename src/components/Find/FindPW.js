1e536b61bc3590edb114b18ee59e219d724c0fbd
seohyunsim <ssh4563@naver.com>
2021-10-01 10:59:16 +0900

    Modify(seohyun/user): 비밀번호 찾기-이메일 전송  api 적용

--- src/components/Find/FindPW.js
+++ src/components/Find/FindPW.js
@@ -1,34 +1,72 @@
-import styles from '../../styles/User/Find/FindPW.module.scss';
-import Link from 'next/link';
+import styles from "../../styles/User/Find/FindPW.module.scss";
+import Link from "next/link";
+import { useState } from "react";
+import axios from "axios";
 
 export const FindPW = () => {
-    return (
-        <div className={styles.wrap}>
-            <div className={styles.find}>
-                <div className={styles.body}>
-                    <h1>비밀번호 찾기</h1>
-                    <input className={styles.num} 
-                        type='text' 
-                        placeholder='학번'
-                    />
-                    <input className={styles.email} 
-                        type='text' 
-                        placeholder='이메일' 
-                    />
-                </div> 
-                <div className={styles.findID}>
-                    <Link href="/findID" passHref>
-                        <span>아이디 찾기</span>
-                    </Link>
-                </div> 
-                <div>
-                    <Link href="/" passHref>
-                        <button>비밀번호 찾기</button>
-                    </Link>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const [id, setId] = useState("");
+  const [email, setEmail] = useState("");
 
-export default FindPW;  
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    name === "id" ? setId(value) : setEmail(value);
+  };
+
+  const onSubmit = () => {
+    axios("http://3.36.72.145:8080/api/forgot-password", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+      },
+      data: {
+        id,
+        email,
+      },
+    })
+      .then((res) => {
+        alert(res.data.msg);
+      })
+      .catch((err) => alert(err.response.data.msg));
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.find}>
+        <div className={styles.body}>
+          <h1>비밀번호 찾기</h1>
+          <input
+            className={styles.num}
+            type="number"
+            placeholder="학번"
+            name="id"
+            onChange={onChange}
+          />
+          <input
+            className={styles.email}
+            type="text"
+            placeholder="이메일"
+            name="email"
+            onChange={onChange}
+            onKeyPress={onKeyPress}
+          />
+        </div>
+        <div className={styles.findID}>
+          <Link href="/findID" passHref>
+            <span>아이디 찾기</span>
+          </Link>
+        </div>
+        <div>
+          <button onClick={onSubmit}>비밀번호 찾기</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FindPW;
