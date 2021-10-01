48046f66b80f6f6b63eb0b888c9be1c52867ee78
Soonki <alstnsrl98@gmail.com>
2021-10-01 10:58:13 +0900

    Modify(soonki/clubhome): 1. 동아리 홈 UI 개선 2. 사이드바 UI 개선 3. 후기 모바일 UI 개선

--- src/components/ClubHome/Intro/Desc.js
+++ src/components/ClubHome/Intro/Desc.js
@@ -1,5 +1,5 @@
-import { useEffect, useRef } from "react";
 import styles from "../../../styles/Club/Home/Intro/Desc.module.scss";
+import { IoPencil } from "react-icons/io5";
 
 const studentId = "201708051";
 
@@ -25,9 +25,12 @@ const Desc = ({
       {leader === studentId ? (
         <div>
           {descUpdate ? (
-            <button onClick={onDescSubnmit}>완료</button>
+            <button onClick={onDescSubnmit}>Finish</button>
           ) : (
-            <button onClick={onDescUpdate}>수정</button>
+            <button onClick={onDescUpdate}>
+              <IoPencil />
+              Edit
+            </button>
           )}
         </div>
       ) : (
