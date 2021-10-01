f7a1d01d42490e3e7f89a819047517c22e0d7cee
parkhyunwoo <pho9902@naver.com>
2021-10-01 13:36:53 +0900

    Refactor(hyunwoo/profile): 개인정보수정 페이지 리팩토링

--- src/components/Profile/ModifyInfo/ModifyInfo.js
+++ src/components/Profile/ModifyInfo/ModifyInfo.js
@@ -1,13 +1,9 @@
 import styles from "../../../styles/Profile/ModifyInfo.module.scss";
-import {
-  FaPhoneAlt,
-  FaCamera,
-  FaUserCircle,
-  FaGraduationCap,
-} from "react-icons/fa";
-import { IoIosMail } from "react-icons/io";
 import { useState } from "react";
 import SetImg from "./SetImg";
+import ModifyHeader from "./ModifyHeader";
+import ImmutableData from "./ImmutableData";
+import MutableData from "./MutableData";
 
 const ModifyInfo = () => {
   const data = {
@@ -36,64 +32,15 @@ const ModifyInfo = () => {
   const [grade, setGrade] = useState(data.profile.grade);
   const [comp, setComp] = useState("수정");
 
-  const gradeArr = [1, 2, 3, 4];
-
   return (
-    <div>
-      <div className={styles.divImg}>
-        <img
-          className={styles.profileImg}
-          src={data.profile.profileImageUrl ?? baseImg}
-        />
-        <FaCamera onClick={() => setComp("이미지수정")} />
-        <hr />
-      </div>
-      <div className={styles.immutable}>
-        <div className={styles.name}>
-          <FaUserCircle />
-          <span> {data.profile.name}</span>
-        </div>
-        <div>
-          <FaGraduationCap />
-          <span>{data.profile.major}</span>
-          <select
-            onChange={(e) => setGrade(e.target.value)}
-            defaultValue={grade}
-          >
-            {gradeArr.map((el, i) => {
-              return (
-                <option key={i} value={el}>
-                  {el === 4 ? "4(졸업생)" : el}
-                </option>
-              );
-            })}
-          </select>
-          <span>학년</span>
-        </div>
-        <p>소속 동아리</p>
-        {data.profile.club.map((el, i) => {
-          return <span key={i}>{el}</span>;
-        })}
-        <hr />
-      </div>
-      <div className={styles.mutable}>
-        <div className={styles.email}>
-          <IoIosMail />
-          <input
-            placeholder={data.profile.email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </div>
-        <div className={styles.phone}>
-          <FaPhoneAlt />
-          <input
-            placeholder={data.profile.phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-        </div>
-      </div>
+    <div className={styles.wrap}>
+      <ModifyHeader data={data} setComp={setComp} baseImg={baseImg} />
+      <ImmutableData data={data} grade={grade} setGrade={setGrade} />
+      <MutableData
+        data={data}
+        setEmail={setEmail}
+        setPhoneNumber={setPhoneNumber}
+      />
       <SetImg comp={comp} setComp={setComp} />
     </div>
   );
