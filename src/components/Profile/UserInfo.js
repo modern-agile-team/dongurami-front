6e6b3a8d8e428732ecf5bafd776f97689cbdaee7
parkhyunwoo <pho9902@naver.com>
2021-10-05 20:21:46 +0900

    Add(hyunwoo/profile): 프로필 조회 GET 요청 api 도입

--- src/components/Profile/UserInfo.js
+++ src/components/Profile/UserInfo.js
@@ -1,41 +1,37 @@
-import react from "react";
-import router from "next/router";
-import styles from "../../styles/Profile/UserInfo.module.scss";
-import { FaGraduationCap, FaUserCircle } from "react-icons/fa";
-import Link from "next/link";
+import react from 'react';
+import router from 'next/router';
+import styles from '../../styles/Profile/UserInfo.module.scss';
+import { FaGraduationCap, FaUserCircle } from 'react-icons/fa';
+import Link from 'next/link';
 
-const UserInfo = ({ logout, comp, setComp, baseImg, data }) => {
-  const moveClub = () => {
-    router.push("/clubhome");
-  };
-
-  if (comp === "프로필")
+const UserInfo = ({ logout, comp, setComp, baseImg, userInfo, profile }) => {
+  if (comp === '프로필')
     return (
       <div>
         <div className={styles.profileBody}>
           <img
             className={styles.profileImg}
-            src={data.profile.profileImageUrl ?? baseImg}
+            src={profile.profileImageUrl ?? baseImg}
           />
           <hr />
           <div>
             <FaUserCircle />
-            <span> {data.profile.name}</span>
+            <span> {profile.name}</span>
           </div>
           <div>
             <FaGraduationCap />
-            <span>{data.profile.major}</span>
-            <span>{data.profile.grade}학년</span>
+            <span>{profile.major}</span>
+            <span>{profile.grade}학년</span>
           </div>
           <p>소속 동아리</p>
-          {data.profile.club.map((club, index) => {
+          {/* {profile.clubs.map((club, index) => {
             return (
-              <span onClick={moveClub} key={index}>
+              <Link href={`/${club.no}/clubhome`} key={index}>
                 {club.title}
-              </span>
+              </Link>
             );
-          })}
-          {data.info.id === data.profile.id ? (
+          })} */}
+          {userInfo.id === profile.id ? (
             <div>
               <Link href="/modifyinfo">
                 <span>개인정보 수정</span>
