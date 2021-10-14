b79aaec254216d21534f35b080e1048ee64f6c95
Soonki <alstnsrl98@gmail.com>
2021-10-14 22:08:49 +0900

    Modify(soonki/clubhome): error handling

--- src/components/ClubLists/ClubList.js
+++ src/components/ClubLists/ClubList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styles from '../../styles/Club/Lists/ClubListContainer.module.scss';
 import { useRouter } from 'next/router';
+import getToken from 'utils/getToken';
 
-const ClubList = ({ img, categories, title, clubNo, clubName }) => {
+const ClubList = ({ img, categories, title, clubNo }) => {
   const router = useRouter();
+
+  const onClick = () => {
+    if (getToken() === '') alert('로그인 후 이용해주세요.');
+    else router.push(`/clubhome/${clubNo}`);
+  };
 
   return (
     <div className={styles.container}>
-      <img
-        src={img}
-        alt="동아리"
-        onClick={() => router.push(`/clubhome/${clubNo}`)}
-      />
+      <img src={img} alt="동아리" onClick={onClick} />
       <div>
         <p id={styles.desc}>{title}</p>
         <p>#{categories}</p>
