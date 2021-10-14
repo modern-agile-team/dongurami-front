ce343ae3620ce593c736ceb8ecb4843a41782290
JunSangYu <junsang.yu3@gmail.com>
2021-10-14 11:32:36 +0900

    Modify(junsang/board): 활동내역 게시판 css 수정

--- src/components/ClubHome/Activities/Act.js
+++ src/components/ClubHome/Activities/Act.js
@@ -4,7 +4,7 @@ import styles from "../../../styles/Club/Home/Activities/Act.module.scss";
 const Act = ({ img, title, onClick, no }) => {
   return (
     <div className={styles.container}>
-      {img && <img src={img} onClick={() => onClick(no)} alt="test" />}
+      <img src={img} onClick={() => onClick(no)} alt="test" />
       <div>
         <p id={styles.desc}>{title}</p>
       </div>
