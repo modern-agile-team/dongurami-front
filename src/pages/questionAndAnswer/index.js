a6be048396b18aec0d11be0263a87414de7e80a9
JunSangYu <junsang.yu3@gmail.com>
2021-10-15 16:32:21 +0900

    Add(junsang/board): qna 게시판 생성

---
+++ src/pages/questionAndAnswer/index.js
@@ -0,0 +1,15 @@
+import Board from "../../components/Board/Board";
+import Footer from "../../components/Common/Footer";
+import Header from "../../components/Common/Header/Header";
+
+function QuestionAndAnswer() {
+  return (
+    <>
+      <Header />
+      <Board category="questionAndAnswer" />
+      <Footer />
+    </>
+  );
+}
+
+export default QuestionAndAnswer;
