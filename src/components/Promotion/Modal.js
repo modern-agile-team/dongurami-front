b5dd4185867c3e2c9eec8c702a1bc39e9bfe185b
Bsfla <baebeomsu77@gmail.com>
2021-10-19 11:45:54 +0900

    Modify(bumsoo/promotion): 게시글  포스터 슬라이더 조회 기능 구현

--- src/components/Promotion/Modal.js
+++ src/components/Promotion/Modal.js
@@ -4,27 +4,27 @@ import Post from './Post';
 import { MdClose } from 'react-icons/md';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import ZoomImg from './ZoomImg';
-import { getdata } from './getdata';
 import { getPost } from 'apis/promotion';
 
 const Modal = ({ setOpenModal, postId }) => {
   const [index, setIndex] = useState(0);
   const [zoom, setZoom] = useState(false);
-  const [imgUrl, setImgUrl] = useState(getdata[index].img);
+  const [images, setImages] = useState([]);
+  const [imgUrl, setImgUrl] = useState('');
   const [postData, setPostData] = useState([]);
   const [comments, setComments] = useState([]);
 
   const nextSlide = () => {
     let idx = index;
 
-    if (idx !== getdata.length - 1) {
+    if (idx !== images.length - 1) {
       idx += 1;
-    } else if (idx === getdata.length - 1) {
+    } else if (idx === images.length - 1) {
       idx = 0;
     }
 
     setIndex(idx);
-    setImgUrl(getdata[index].img);
+    setImgUrl(images[index].imgPath);
   };
 
   const prevSlide = () => {
@@ -32,16 +32,17 @@ const Modal = ({ setOpenModal, postId }) => {
 
     if (idx === 0) idx = getdata.length - 1;
     else idx -= 1;
-
     setIndex(idx);
-    setImgUrl(getdata[index].img);
+    setImgUrl(images[index].imgPath);
   };
 
   const getData = async () => {
     try {
       await getPost(postId).then((res) => {
         if (res.data.success) {
-          console.log(res);
+          console.log(res.data.images[0].imgPath);
+          setImages(res.data.images);
+          setImgUrl(res.data.images[index].imgPath);
           setPostData(res.data.board);
           setComments(res.data.comments);
         } else alert(res.data.msg);
@@ -67,9 +68,9 @@ const Modal = ({ setOpenModal, postId }) => {
       {!zoom && (
         <>
           <div className={styles.image} onClick={(e) => e.stopPropagation()}>
-            <IoIosArrowBack size={70} onClick={prevSlide} />
+            <IoIosArrowBack onClick={() => prevSlide()} size={70} />
             <img src={imgUrl} onClick={() => setZoom(true)} />
-            <IoIosArrowForward size={70} onClick={nextSlide} />
+            <IoIosArrowForward onClick={() => nextSlide()} size={70} />
           </div>
           <Post
             postData={postData}
