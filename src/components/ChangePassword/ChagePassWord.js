b01ef464398418b88db1fb461b55e1c0a65a684f
seohyunsim <ssh4563@naver.com>
2021-10-27 14:52:37 +0900

    Add(seohyun/user): 비밀번호 변경 페이지 기능 구현 및 api적용

--- src/components/ChangePassword/ChagePassWord.js
+++ src/components/ChangePassword/ChagePassWord.js
@@ -1,34 +1,79 @@
-import styles from "../../styles/User/ChangePassword/ChangePassword.module.scss";
+import styles from '../../styles/User/ChangePassword/ChangePassword.module.scss';
+import { useState } from 'react';
+import getToken from 'utils/getToken';
+import { patchChangePW } from 'apis/user';
 
 function ChangePW() {
+  const [password, setPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [checkNewPassword, setCheckNewPassword] = useState('');
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'password') setPassword(value);
+    if (name === 'newPassword') setNewPassword(value);
+    if (name === 'checkNewPassword') setCheckNewPassword(value);
+  };
+
   const onSubmit = () => {
-    e.preventdefault();
-    axios("http://3.36.72.145:8080/api/reset-password", {
-      method: "PATCH",
-      headers: {
-        "Content-type": "application/json; charset=utf-8",
-      },
-      data: {
+    patchChangePW(
+      {
         password,
         newPassword,
-        checkNewPassword,
+        checkNewPassword
       },
-    })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+      {
+        headers: {
+          'Content-type': 'application/json; charset=utf-8',
+          'x-auth-token': getToken()
+        }
+      }
+    )
+      .then((res) => alert(res.data.msg))
+      .catch((err) => alert(err.response.data.msg));
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onSubmit();
+    }
   };
 
   return (
     <div className={styles.container}>
-      <h1>비밀번호 변경</h1>
-      <form>
-        <input type="password" placeholder="기존 비밀번호 입력" />
-        <input type="password" placeholder="변경할 비밀번호 입력" />
-        <input type="password" placeholder="변경할 비밀번호 입력 확인" />
-        <button type="submit" onClick={onSubmit}>
-          변경하기
-        </button>
-      </form>
+      <div className={styles.changePW}>
+        <div className={styles.body}>
+          <h1>비밀번호 변경</h1>
+          <input
+            type="password"
+            placeholder="기존 비밀번호"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            placeholder="변경할 비밀번호"
+            name="newPassword"
+            value={newPassword}
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            placeholder="변경할 비밀번호 확인"
+            name="checkNewPassword"
+            value={checkNewPassword}
+            onChange={onChange}
+            onKeyPress={onKeyPress}
+          />
+        </div>
+        <div className={styles.btnWrap}>
+          <button type="submit" onClick={onSubmit}>
+            변경하기
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
