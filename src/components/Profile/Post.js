c2ab87e43d0fe039cfbc50c1bb20fd3df5f96539
unknown <pho9902@naver.com>
2021-11-01 13:56:21 +0900

    Fix(hyunwoo/profile): 마이페이지 스크랩글 삭제 오류 수정

---
+++ src/components/Profile/Post.js
@@ -0,0 +1,55 @@
+import CommentContainer from 'components/Common/Comment/CommentContainer';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from '../../styles/Board/Post/PostContent.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { setCategory } from 'redux/slices/post';
+import moment from 'moment';
+
+const ReactQuill = dynamic(import('react-quill'), {
+  ssr: false
+});
+
+function Post({ category, post, onDelete, editLink }) {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(setCategory(category));
+  }, [category, router, dispatch]);
+
+  const clubNum = Number(router.query.id);
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <a>활동내용</a>
+        <h1>{post.title}</h1>
+        <div>
+          <Link href={`/profile/${post.studentId}`} passHref>
+            <div>{post.name}</div>
+          </Link>
+          <div>
+            {user?.id === post.studentId && (
+              <>
+                <Link href={editLink} passHref>
+                  <button>수정하기</button>
+                </Link>
+                <button onClick={onDelete}>삭제하기</button>
+              </>
+            )}
+            <div>{moment(post.inDate).format('YYYY-MM-DD')}</div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <ReactQuill value={post.description} theme="bubble" readOnly />
+      {post.comments && <CommentContainer comments={post.comments} />}
+    </div>
+  );
+}
+
+export default Post;
