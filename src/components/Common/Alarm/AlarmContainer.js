980af91b93be8b79503a651b159ec7a74be12de7
Soonki <alstnsrl98@gmail.com>
2021-11-02 13:34:50 +0900

    Modify(soonki/alarm): 알람 수정사항 적용

--- src/components/Common/Alarm/AlarmContainer.js
+++ src/components/Common/Alarm/AlarmContainer.js
@@ -1,6 +1,7 @@
 import styles from '../../../styles/Common/Alarm/AlarmContainer.module.scss';
 import { FaTrashAlt } from 'react-icons/fa';
 import AlarmBody from './AlarmBody';
+import { useEffect, useState } from 'react';
 
 const AlarmContainer = ({
   alarmList,
@@ -10,14 +11,24 @@ const AlarmContainer = ({
   alarmShow,
   getAlarmData
 }) => {
+  const [isDelete, setIsDelete] = useState(false);
+
+  useEffect(() => {
+    if (isDelete === true) setTimeout(() => setIsDelete(false), 2000);
+  }, [isDelete]);
+
   return (
     <>
       <div className={styles.rect} />
       <div className={styles.container}>
         <div className={styles.delete}>
-          <FaTrashAlt size={13} onClick={onAlarmDeleteAll} />
+          <div>{isDelete && <span>알람이 삭제되었습니다</span>}</div>
+          <div>
+            <FaTrashAlt size={13} onClick={onAlarmDeleteAll} />
+          </div>
         </div>
         <AlarmBody
+          setIsDelete={setIsDelete}
           alarmList={alarmList}
           alarmShow={alarmShow}
           showMoreAlarm={showMoreAlarm}
