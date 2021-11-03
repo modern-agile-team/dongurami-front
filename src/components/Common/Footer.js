42a79b62961469542dd6f96a4bdcf1868f99aa53
seohyunsim <ssh4563@naver.com>
2021-11-03 15:23:14 +0900

    Fix(seohyun/footer): tooltip 관련 경고창 이슈 수정

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -2,8 +2,15 @@ import React from 'react';
 import styles from '../../styles/Common/Footer.module.scss';
 import { SiNotion } from 'react-icons/si';
 import ReactTooltip from 'react-tooltip';
+import { useState, useEffect } from 'react';
 
 export default function Footer() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   const goToNotion = () => {
     window.open(
       'https://www.notion.so/e30b5df25a044809823784f0fee40686',
@@ -24,26 +31,25 @@ export default function Footer() {
     <div className={styles.footer}>
       <div className={styles.wrap}>
         <div className={styles.btn}>
+          {isMounted && <ReactTooltip effect="solid" />}
           <SiNotion
             onClick={goToNotion}
             className={styles.notion}
-            data-tip="우아한 애자일 소개"
-            data-type="light"
-            data-border="true"
-            data-border-color="#bbbbbb"
+            data-tip={'우아한 애자일 소개'}
+            data-type={'light'}
+            data-border={'true'}
+            data-border-color={'#bbbbbb'}
           />
-          <ReactTooltip effect="solid" />
           <img
             onClick={goToInduk}
             className={styles.induk}
-            data-tip="인덕대학교"
-            data-place="bottom"
-            data-type="light"
-            data-border="true"
-            data-border-color="#bbbbbb"
+            data-tip={'인덕대학교'}
+            data-place={'bottom'}
+            data-type={'light'}
+            data-border={'true'}
+            data-border-color={'#bbbbbb'}
             src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/HomePage/induk-icon.png"
           />
-          <ReactTooltip effect="solid" />
         </div>
         <div className={styles.copyright}>
           <span>&copy; {thisYear()} Wooahan Agile</span>
