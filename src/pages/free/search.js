ed016f2de2a9997a3d5f2aba26db9db3748f3bca
Soonki <alstnsrl98@gmail.com>
2021-11-03 14:12:27 +0900

    Add(soonki/detail): title 추가

--- src/pages/free/search.js
+++ src/pages/free/search.js
@@ -1,10 +1,14 @@
-import Header from "../../components/Common/Header/Header";
-import Footer from "../../components/Common/Footer";
-import Board from "components/Board/Board";
+import Header from '../../components/Common/Header/Header';
+import Footer from '../../components/Common/Footer';
+import Board from 'components/Board/Board';
+import Head from 'next/head';
 
 function search() {
   return (
     <>
+      <Head>
+        <title>동그라미 | 게시글 검색</title>
+      </Head>
       <Header />
       <Board
         category="free"
--- src/pages/promotion/index.js
+++ src/pages/promotion/index.js
@@ -1,9 +1,13 @@
 import PromotionContainer from '../../components/Promotion/PromotionContainer';
 import Footer from '../../components/Common/Footer';
+import Head from 'next/head';
 
 function promotion() {
   return (
     <>
+      <Head>
+        <title>동그라미 | 홍보 게시판</title>
+      </Head>
       <PromotionContainer />
       <Footer />
     </>
