1abe70dc2eeb5ebf265c839e2ac5214f4cd51d80
seohyunsim <ssh4563@naver.com>
2021-11-04 16:05:35 +0900

    Refactor(seohyun/img): 이미지 태그 alt 속성 추가

--- src/components/main/OurIntro.js
+++ src/components/main/OurIntro.js
@@ -6,7 +6,10 @@ const OurIntro = () => {
   return (
     <div className={styles.wrap}>
       <Fade>
-        <img src="https://lovelyoch.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd4fd10ea-d2b7-4777-a4cb-d4e5ccbf1f63%2F%EC%9A%B0%EC%95%A0_%EC%A0%84%EC%B2%B4%EC%83%B7.jpg?table=block&id=9ab79fa2-a9c5-41cc-a891-cbe204a334d2&spaceId=69eb8ea8-3d04-47ec-8bb7-004e8aa31f9e&width=2880&userId=&cache=v2" />
+        <img
+          alt="우아한 애자일 2기"
+          src="https://lovelyoch.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd4fd10ea-d2b7-4777-a4cb-d4e5ccbf1f63%2F%EC%9A%B0%EC%95%A0_%EC%A0%84%EC%B2%B4%EC%83%B7.jpg?table=block&id=9ab79fa2-a9c5-41cc-a891-cbe204a334d2&spaceId=69eb8ea8-3d04-47ec-8bb7-004e8aa31f9e&width=2880&userId=&cache=v2"
+        />
         <div className={styles.text}>
           <h1>
             SW 개발 전문 동아리
