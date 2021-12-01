67e3e8f892c947f23e73a02b57c18a505f06b56b
JunSangYu <junsang.yu3@gmail.com>
2021-12-01 12:46:07 +0900

    Modify(junsang/board): 공지, 동아리공지, 동아리활동 게시판 익명 금지

--- src/components/Write/WriteContent.js
+++ src/components/Write/WriteContent.js
@@ -2,13 +2,14 @@ import styles from "../../styles/Board/Write/WriteContent.module.scss";
 import ReactQuillContainer from "./ReactQuillContainer";
 
 function Write({ category, title, description, isAnon, setTitle, setDescription, setIsAnon, onSubmit }) {
+  console.log(category);
   return (
     <div className={styles.container}>
       <input type="text" placeholder="제목을 입력하세요..." value={title} onChange={(e) => setTitle(e.target.value)} />
       <hr />
       <ReactQuillContainer description={description} setDescription={setDescription} />
       <div>
-        {(category !== 'promotion') && (<label>
+        {(category !== 'promotion' && category !== 'notice' && category !== 'clubNotice' && category !== 'clubActivity') && (<label>
           익명여부:
           <input type="checkbox" checked={isAnon} onChange={() => setIsAnon(!isAnon)} />
         </label>)}
