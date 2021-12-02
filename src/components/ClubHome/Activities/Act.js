6a515cf543dd7fa115c7d33941667cda1f2a918f
JunSangYu <junsang.yu3@gmail.com>
2021-12-02 17:10:02 +0900

    Modify(junsang/board): 동아리 활동내역 좋아요 미리보기 추가

--- src/components/ClubHome/Activities/Act.js
+++ src/components/ClubHome/Activities/Act.js
@@ -1,13 +1,20 @@
-import React from "react";
-import styles from "../../../styles/Club/Home/Activities/Act.module.scss";
+import React from 'react';
+import styles from '../../../styles/Club/Home/Activities/Act.module.scss';
 
 const Act = ({ post, onClick }) => {
   return (
     <div className={styles.container} onClick={() => onClick(post.no)}>
-      <img src={post.url ?? 'https://alisebodycare.com/wp-content/uploads/2017/05/image-placeholder-500x500.jpg'} alt="thumbnail" />
+      <img
+        src={
+          post.url ??
+          'https://alisebodycare.com/wp-content/uploads/2017/05/image-placeholder-500x500.jpg'
+        }
+        alt="thumbnail"
+      />
       <div>
         <p className={styles.title}>{post.title}</p>
-        <p className={styles.name}>{post.studentName}</p>
+        <span className={styles.name}>{post.studentName}</span>
+        <span className={styles.emotion}>♡{post.emotionCount}</span>
       </div>
     </div>
   );
