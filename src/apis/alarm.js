667caf41453f3c19a4ce159ce5a6cbadc250ac0d
Min <alstnsrl98@gmail.com>
2022-01-07 11:15:12 +0900

    Refactor(soonki/alarm): rename deleting alarm API

--- src/apis/alarm.js
+++ src/apis/alarm.js
@@ -6,12 +6,12 @@ export const getAlarm = () => {
 };
 
 // 알람 전체 삭제
-export const putAlarm = () => {
+export const deleteAllAlarm = () => {
   return instance.put(`api/notification/entire`);
 };
 
 // 알람 하나 삭제
-export const patchAlarm = (data) => {
+export const deleteOneAlarm = (data) => {
   return instance.patch(`api/notification/${data}`);
 };
 
